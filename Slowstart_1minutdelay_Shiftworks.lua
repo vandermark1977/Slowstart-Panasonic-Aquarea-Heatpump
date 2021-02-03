6df5752da0e5acb4d69bebf79798bb9277111f61
vandermark1977 <49677940+vandermark1977@users.noreply.github.com>
2021-02-03 11:47:05 +0100

    Update Slowstart_1minutdelay_Shiftworks.lua

--- Slowstart_1minutdelay_Shiftworks.lua
+++ Slowstart_1minutdelay_Shiftworks.lua
@@ -15,7 +15,9 @@ return {
         -- 2: compressor startup
         -- 3: compressor relaxing
         -- 4: compressor continuous operation
-        state = { initial = 1 }
+        ---------------------------------------
+        state = { initial = 1 },
+        treshold = {initial = 20}
     },
     logging = {
         level = domoticz.LOG_DEBUG, -- change to LOG_ERROR when OK - was LOG_DEBUG
@@ -28,7 +30,30 @@ return {
         local CompressorFreq = domoticz.devices(49) -- Fill in IDX of the Pana [Compressor_Freq]
         local Toggle = domoticz.devices(148)        -- Fill in IDX of On/Off switch you need to create from a dummy device. This on/of switch is only used for this script
         local ShiftManual = domoticz.devices(149)   -- Fill in IDX of Your Manual TaShift [temperature thermostat]
-
+----------------------------------------------
+-- Determine treshold for compressofrequency--
+-- To set change from state 3 --> 4         --
+-- Adjust these to your own situation!!     --
+----------------------------------------------
+        if (target_temp.temperature == 26) then
+            domoticz.data.treshold = 25 end
+        if (target_temp.temperature == 27) then
+            domoticz.data.treshold = 26 end
+        if (target_temp.temperature == 28) then
+            domoticz.data.treshold = 27 end
+        if (target_temp.temperature == 29) then
+            domoticz.data.treshold = 27 end
+        if (target_temp.temperature == 30) then
+            domoticz.data.treshold = 28 end
+        if (target_temp.temperature == 31) then
+            domoticz.data.treshold = 28 end
+        if (target_temp.temperature == 32) then
+            domoticz.data.treshold = 28 end
+        if (target_temp.temperature == 33) then
+            domoticz.data.treshold = 28 end
+---------------------------
+-- Slowstart starts here --
+---------------------------
         if(CompressorFreq.sValue == "0") then
             domoticz.log('State: compressor off', domoticz.LOG_INFO)
             domoticz.data.state = 1
@@ -37,49 +62,53 @@ return {
             domoticz.log('State: compressor startup', domoticz.LOG_INFO)
             domoticz.data.state = 2
             correction = outlet_temp.temperature - target_temp.temperature + heatshift.setPoint -1
-            if(tonumber(CompressorFreq.sValue) >= 26) then
+            if(tonumber(CompressorFreq.sValue) >= domoticz.data.treshold) then
                 domoticz.data.state = 3
             end
         elseif(domoticz.data.state == 3) then
             domoticz.log('State: compressor relaxing', domoticz.LOG_INFO)
             correction = outlet_temp.temperature - target_temp.temperature + heatshift.setPoint -1
-            if((outlet_temp.temperature - target_temp.temperature) >= 1) and (tonumber(CompressorFreq.sValue) < 26) then
+            if((outlet_temp.temperature - target_temp.temperature) >= 1) and (tonumber(CompressorFreq.sValue) < domoticz.data.treshold) then
                 domoticz.data.state = 4
             end
         elseif(domoticz.data.state == 4) then
-            domoticz.log('State: continu status', domoticz.LOG_INFO)
+            domoticz.log('State: continuous status', domoticz.LOG_INFO)
             if((outlet_temp.temperature - target_temp.temperature) >= 0) then
                 correction = heatshift.setPoint + 1
-                domoticz.log('TaDoel is: '.. target_temp.temperature .. ' & Ta is: '.. outlet_temp.temperature.. ': Continu met Correctie is Shift+1: (' .. tostring(correction)..')', domoticz.LOG_INFO)
+                domoticz.log('TaDoel is: '.. target_temp.temperature .. ' & Ta is: '.. outlet_temp.temperature.. ': Continuous with correction is Shift+1: (' .. tostring(correction)..')', domoticz.LOG_INFO)
             else
                 correction = heatshift.setPoint
-                domoticz.log('Continu zonder aanpassing: TaDoel is: '.. target_temp.temperature .. ' & Ta is: '.. outlet_temp.temperature, domoticz.LOG_INFO)
+                domoticz.log('Continuos without adjustment: TaTarget is: '.. target_temp.temperature .. ' & Ta is: '.. outlet_temp.temperature, domoticz.LOG_INFO)
             end
         else
             domoticz.log('State: undefined', domoticz.LOG_INFO)
             domoticz.data.state = 1
             correction = 0
         end
-        
+-----------------------------------------------------------------------
+-- Correction is translated to Shift Target temperature water outlet --
+-----------------------------------------------------------------------
         if correction < -5 then
             correction = -5 end
         if correction > ShiftManual.setPoint then 
-            domoticz.log('Correctie ('.. tostring(correction)..') groter dan ShiftManual (' .. ShiftManual.setPoint..'): Correctie gezet op: ' .. ShiftManual.setPoint, domoticz.LOG_INFO)
+            domoticz.log('Correction ('.. tostring(correction)..') above ShiftManual (' .. ShiftManual.setPoint..'): Correction set to: ' .. ShiftManual.setPoint, domoticz.LOG_INFO)
             correction = ShiftManual.setPoint end
         
         if heatshift.setPoint == correction then
-            domoticz.log('Geen correctie: Shift is al gelijk aan correctie ('.. tostring(correction).. ')', domoticz.LOG_INFO) end
+            domoticz.log('No correction: Current Shift equals correction ('.. tostring(correction).. ')', domoticz.LOG_INFO) end
         
         if (heatshift.setPoint ~= correction) and (Toggle.lastUpdate.minutesAgo >= 1) then
-            domoticz.log('Correctie nodig en is gezet op: ' .. tostring(correction), domoticz.LOG_INFO)
+            domoticz.log('Correction needed and set to: ' .. tostring(correction), domoticz.LOG_INFO)
             heatshift.updateSetPoint(correction)
             Toggle.toggleSwitch() end
         
         if (heatshift.setPoint ~= correction) and (Toggle.lastUpdate.minutesAgo < 1) then
-            domoticz.log('Correctie nodig maar niet uitgevoerd, laatste correctie korter dan minuut geleden ', domoticz.LOG_INFO)
-        
+            domoticz.log('Correction needed but not executed, last correction < 1 minute ago', domoticz.LOG_INFO)
+---------------------------------------
+-- Final Log                         --
+---------------------------------------
         else
-            domoticz.log('Einde script. Toggle laatst getriggerd: ' .. Toggle.lastUpdate.minutesAgo..' minuten geleden', domoticz.LOG_INFO)
+            domoticz.log('End script. Toggle last triggerd: ' .. Toggle.lastUpdate.minutesAgo..' minutes ago. Treshold is: '..domoticz.data.treshold, domoticz.LOG_INFO)
         end
     end
 }
